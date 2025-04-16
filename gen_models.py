d211abc2666e1036817a05a36c55f67551893e64
Donovan <donovan.sproule@yahoo.com>
2025-04-16 19:36:33 -0400

    metadata tracking developed

--- gen_models.py
+++ gen_models.py
@@ -4,7 +4,7 @@ import json
 from qkeras import QDense, QConv2D, QConv1D, QAveragePooling2D, QActivation, quantized_bits, QDepthwiseConv2D, QSeparableConv2D, QSeparableConv1D, QLSTM
 from keras.layers import Dense, Conv2D, Flatten, Activation, Conv1D, LSTM, Layer, Input
 from keras.models import Model, model_from_json
-from qkeras.utils import _add_supported_quantized_objects
+# from qkeras.utils import _add_supported_quantized_objects
 import keras
 import typing
 import random
@@ -15,6 +15,8 @@ from contextlib import contextmanager
 import sys, os
 import ray
 from ray.exceptions import RayTaskError
+from qkeras.quantizers import quantized_bits
+from qkeras import Clip
 
 import logging
 
@@ -30,6 +32,45 @@ def suppress_stdout():
             sys.stdout = old_stdout
 
 clip_base_2 = lambda x: 2 ** round(np.log2(x))
+
+def _add_supported_quantized_objects(co: dict):
+
+    co.update({
+        "QDense": QDense,
+        "QConv2D": QConv2D,
+        "QConv1D": QConv1D,
+        "QActivation": QActivation,
+        "QAveragePooling2D": QAveragePooling2D,
+        "QDepthwiseConv2D": QDepthwiseConv2D,
+        "QSeparableConv2D": QSeparableConv2D,
+        "QSeparableConv1D": QSeparableConv1D,
+        "quantized_bits": quantized_bits,
+        "Clip": Clip,
+        "Functional": Model,
+    })
+
+def sanitize_model_json(json_str: str) -> str:
+    model_config = json.loads(json_str)
+
+    def clean_config(layer):
+        layer_config = layer.get("config", {})
+        if layer["class_name"] == "QDepthwiseConv2D":
+            for key in ["groups", "kernel_regularizer", "kernel_constraint"]:
+                layer_config.pop(key, None)
+
+        elif layer["class_name"] == "QSeparableConv2D":
+            for key in [
+                "groups",
+                "kernel_initializer",  # <-- REMOVE THIS TOO
+                "kernel_regularizer",
+                "kernel_constraint"
+            ]:
+                layer_config.pop(key, None)
+
+    for layer in model_config.get("config", {}).get("layers", []):
+        clean_config(layer)
+
+    return json.dumps(model_config)
 
 class Model_Generator:
     failed_models = 0
@@ -54,7 +95,7 @@ class Model_Generator:
             hyper_params = {'size': layer_size, 'activation': activation, 'use_bias': use_bias,
                             'dropout': dropout, 'dropout_rate': self.params['dropout_rate']}
         elif layer_type in self.conv_layers:
-            out_filters = clip_base_2(random.randint(self.params['conv_filters_lb'], self.params['conv_filters_ub']))
+            out_filters = clip_base_2(random.randint(3, 256))
             flatten = (random.random() < self.params['flatten_chance']) or \
                       (self.params['last_layer_shape'][0] < self.params['conv_flatten_limit'] or
                        self.params['last_layer_shape'][1] < self.params['conv_flatten_limit'])
@@ -75,15 +116,15 @@ class Model_Generator:
                             'flatten': flatten, 'activation': activation, 'use_bias': use_bias,
                             'pooling': pooling, 'padding': padding, 'stride': (stride, stride)}
         elif layer_type in self.time_layers:
-            out_filters = clip_base_2(random.randint(self.params['conv_filters_lb'], self.params['conv_filters_ub']))
+            out_filters = clip_base_2(random.randint(3, 256))
             kernel_size = random.randint(self.params['conv_kernel_lb'], self.params['conv_kernel_ub'])
             flatten = random.random() < self.params['flatten_chance']
             stride = random.randint(self.params['conv_stride_lb'], self.params['conv_stride_ub'])
             padding = random.choices(['same', 'valid'], weights=self.params['probs']['padding'], k=1)[0]
-            pooling = random.random() < self.params['pooling_chance']
+
             hyper_params = {'out_filters': out_filters, 'kernel': kernel_size,
                             'flatten': flatten, 'activation': activation, 'use_bias': use_bias,
-                            'pooling': pooling, 'padding': padding, 'stride': stride}
+                            'padding': padding, 'stride': stride}
         return hyper_params
 
     def next_layer(self, last_layer: Layer, input_layer: Layer = None, pre_config: dict = None) -> Layer:
@@ -144,9 +185,7 @@ class Model_Generator:
                     layer_choice.append(QActivation(activation=hyper_params['activation'])(layer_choice[-1]))
 
                 if hyper_params['pooling']:
-                    pooling = random.choices([keras.layers.MaxPooling2D, keras.layers.AveragePooling2D],
-                                             weights=self.params['probs']['pooling'], k=1)[0]
-                    layer_choice.append(pooling((2, 2))(layer_choice[-1]))
+                    layer_choice.append(QAveragePooling2D()(layer_choice[-1]))
             else:
                 layer_choice = [layer_type(hyper_params['out_filters'], hyper_params['kernel'], strides=hyper_params['stride'],
                                            use_bias=hyper_params['use_bias'], padding=hyper_params['padding'])(last_layer)]
@@ -188,10 +227,6 @@ class Model_Generator:
                 
                 if "no_activation" not in hyper_params['activation']:
                     layer_choice.append(QActivation(activation=hyper_params['activation'])(layer_choice[-1]))
-                if hyper_params['pooling']:
-                    pooling = random.choices([keras.layers.MaxPooling1D, keras.layers.AveragePooling1D],
-                                             weights=self.params['probs']['pooling'], k=1)[0]
-                    layer_choice.append(pooling(2)(layer_choice[-1]))
             else:
                 if layer_type == LSTM:
                     raise NotImplemented
@@ -202,10 +237,6 @@ class Model_Generator:
                     
                 if "no_activation" not in hyper_params['activation']:
                     layer_choice.append(Activation(activation=hyper_params['activation'])(layer_choice[-1]))
-                if hyper_params['pooling']:
-                    pooling = random.choices([keras.layers.MaxPooling1D, keras.layers.AveragePooling1D],
-                                             weights=self.params['probs']['pooling'], k=1)[0]
-                    layer_choice.append(pooling(2)(layer_choice[-1]))
             self.name = 'time'
             if hyper_params['flatten'] and input_layer is None:
                 layer_choice.append(Flatten()(last_layer))
@@ -308,8 +339,10 @@ class Model_Generator:
             model = Model(inputs=layers[0], outputs=layers[-1])
         
             if save_file:
-                save_file.write(model.to_json())
-                save_file.write("--------------")
+                json_models = json.dumps(model, indent=None)
+                with open(save_file, "w") as file:
+                    file.write(json_models)
+                
             return model
         
         except ValueError as e:
@@ -367,20 +400,27 @@ class Model_Generator:
             if p_type not in self.params['probs']:
                 self.params['probs'][p_type] = [.5, .5]
 
-    def load_models(self, save_file: str) -> list[Model]:
+    def load_models(self, save_file: str) -> list:
         """
-        Parses and returns an iterable of generated models
-        
-        arguments:
-        save_file -- path to batch of models
+        Parses and returns an iterable of generated models from a JSON lines file,
+        where each line contains one or more models as stringified Keras JSON configs.
+        Expects a comma separated list.
+
+        Arguments:
+        save_file -- path to JSONL file
         """
 
-        with open(save_file, "r") as chunk_file:
-            models = chunk_file.read().split("--------------")[:-1]
-            for model_desc in models:
-                co = {}
-                _add_supported_quantized_objects(co)
-                yield model_from_json(model_desc, custom_objects=co)
+        with open(save_file, "r") as f:
+            json_model_list = json.load(f)
+
+        for model_label in json_model_list:
+            model_json = json_model_list[model_label]
+            
+            co = {}
+            _add_supported_quantized_objects(co)
+
+            cleaned_json = sanitize_model_json(model_json)
+            yield model_from_json(cleaned_json, custom_objects=co)
 
 ray.init(num_cpus=os.cpu_count(), log_to_driver=False)
 
@@ -388,63 +428,77 @@ ray.init(num_cpus=os.cpu_count(), log_to_driver=False)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 @ray.remote(max_retries=10, retry_exceptions=True)
-def generate_model(bitwidth):
+def generate_model(bitwidth, mg, params):
     try:
-        mg = Model_Generator()
-        model = mg.gen_network(add_params={'dense_lb': 8, 'dense_ub': 64, 'conv_filters_ub': 16,
-                                           'conv_init_size_lb': 8, 'conv_init_size_ub': 64,
-                                           'q_chance': 1, 'flatten_chance': .1, 'pooling_chance': .3,
-                                           'weight_bit_width': bitwidth, 'weight_int_width': 1,
-                                           'activ_bit_width': bitwidth, 'activ_int_width': 1,
-                                           'probs': {'activations': [],
-                                                     # Must set probabilities for the layers in start_layers as well!
-                                                    # conv layers
-                                                    # q_chance = 0 [Conv2D]
-                                                    # q_chance < 1 [Conv2D, QConv2D, QSeparableConv2D, QDepthwiseConv2D]
-                                                    # q_chance = 1 [QConv2D, QSeparableConv2D, QDepthwiseConv2D]
-                                                    # Dense/Time are either qkeras or not in line with q_chance,
-                                                    # 1 element if 0/1, else 2 elements
-                                                     'dense_layers': [.25], 'conv_layers': [0.75, 0, 0], 'time_layers': [0],
-                                                     # start layers
-                                                     #q_chance = 0 [Conv1D, Conv2D, Dense]
-                                                     #q_chance < 1 [Conv1D, QConv1D, Conv2D, QConv2D, QDense, Dense, QSeparableConv2D, QDepthwiseConv2D]
-                                                     #q_chance = 1 [QConv1D, QConv2D, QDense, QSeparableConv2D, QDepthwiseConv2D]
-                                                     'start_layers': [0, 1, 0, 0, 0],
-                                                     'padding': [0.5, 0.5],  # border, off
-                                                     'pooling': [0.2, 0.2]  # max, avg
-                                                     }
-                                           },
-                               total_layers=random.randint(3, 7), save_file=None)
+        model = mg.gen_network(add_params=params,
+                            total_layers=random.randint(3, 10), save_file=None)
         return model.name, model.to_json()
     except Exception as e:
+        mg.failed_models += 1
         raise(e)
 
-def threaded_exec(batch_range: int, batch_size: int):
+def threaded_exec(batch_range: int, batch_size: int, params: dict = {}):
     succeeded = 0
 
     assert batch_range > 0
     assert batch_size > 0
 
     for batch_i in tqdm(range(batch_range), desc="Batch Count:"):
-        model_dict = {}
-        futures = [generate_model.remote(2 ** random.randint(2, 4)) for _ in range(batch_size)]
+        models = []
+        futures = [generate_model.remote(2 ** random.randint(2, 4), Model_Generator(), params) for _ in range(batch_size)]
         for future in ray.get(futures):
             model_name, model_json = future
             if model_name and model_json:
-                # model_name might have dupes because of multithreading, so make a new name for each model
-                model_dict.update({f"conv2d_{succeeded}": model_json})  # Store the model with its name
-                succeeded += 1
-        json_models = json.dumps(model_dict)
-        with open(f"conv2d_models/conv2d_batch_{batch_i}.json", "w") as file:
+                models.append(model_json)
+                succeeded +=1
+        json_models = json.dumps(models, indent=None, separators=(',', '\n'))
+        with open(f"conv2d_batch_{batch_i}.json", "w") as file:
             file.write(json_models)
+        print(succeeded)
+        print(len(models))
 
-
+from collections import defaultdict
 if __name__ == '__main__':
-    batch_range = 668
-    batch_size = 50
-    threaded_exec(batch_range, batch_size)
+    # threaded_exec(batch_range, batch_size)
     
-    # left this here as an example but everything beyond this line in __name__ can be deleted
+    # left this here as an example but everything in __name__ can be deleted
     def callback(mg: Model_Generator, layers: list):
         if mg.layer_depth > 1:
             mg.params['flatten_chance'] = 1
+
+    mg = Model_Generator()
+    model_data = {}
+    for batch_file in os.listdir('conv2d_models'):
+        for model in mg.load_models(f"conv2d_models/{batch_file}"):
+            # get number of layers
+            layer_cnt = 0
+            cur_data = defaultdict(int)
+            for layer in model.layers:
+                if 'conv' in layer.name or 'dense' in layer.name:
+                    if 'conv1d' in layer.name:
+                        cur_data['conv1d'] += 1
+                    if 'conv2d' in layer.name:
+                        cur_data['conv2d'] += 1
+                    if 'dense' in layer.name:
+                        cur_data['dense'] += 1
+                    layer_cnt += 1
+                elif 'activation' in layer.name:
+                    cur_data['activation'] += 1
+                elif 'pool' in layer.name:
+                    cur_data['pool'] += 1
+
+            if layer_cnt not in model_data:
+                model_data[layer_cnt] = defaultdict(int)
+            model_data[layer_cnt]['model_cnt'] += 1
+            model_data[layer_cnt]['conv'] += cur_data['conv2d'] + cur_data['conv1d']
+            model_data[layer_cnt]['dense'] += cur_data['dense']
+            model_data[layer_cnt]['conv1d'] += cur_data['conv1d']
+            model_data[layer_cnt]['conv2d'] += cur_data['conv2d']
+            model_data[layer_cnt]['activation'] += cur_data['activation']
+            model_data[layer_cnt]['pooling'] += cur_data['pool']
+            model_data[layer_cnt]['only_dense'] += int(cur_data['conv2d'] == 0 and cur_data['conv1d'] == 0)
+
+    # saves the metadata
+    serializable_model_data = {k: dict(v) for k, v in model_data.items()}
+    with open("model_layer_metadata.json", "w") as f:
+        json.dump(serializable_model_data, f, indent=2)
